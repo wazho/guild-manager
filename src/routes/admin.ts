eedb826ce9c69a2ae905ae7d036fead87e7ed4c1
Ze-Hao Wang (a.k.a. Salmon) <salmon.zh.tw@gmail.com>
2018-09-24 21:06:04 +0800

    (^) Add link for admin page.

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -2,44 +2,61 @@
 import * as Router from 'koa-router';
 // Local modules.
 import { webAppURL } from '../config';
+import { findUser } from '../libs/google-apis';
 import { renderHtml } from '../libs/render-html';
 
 const router = new Router();
 
-router.get('/', (ctx, next) => {
-    try {
+async function levelIdentify(ctx: Router.IRouterContext, callback: any) {
+    const { user } = ctx.state;
+    const remoteUser = await findUser(user && user.lineID);
+
+    if (remoteUser) {
+        const legalLevels = ['公會長', '副會長'];
+
+        if (legalLevels.includes(remoteUser.status)) {
+            callback();
+        } else {
+            ctx.redirect('/');
+        }
+    } else {
+        ctx.redirect('/login');
+    }
+}
+
+router.get('/', async (ctx, next) => {
+    await levelIdentify(ctx, () => {
         const path = './src/views/admin/root.pug';
         const html = renderHtml(path);
         ctx.body = html;
-    } catch (e) {
-        console.log(e);
-        return ctx.redirect('/');
-    }
+    });
 });
 
-router.post('/invite-code', (ctx, next) => {
-    const body = ctx.request.body;
-
-    if (body) {
-        // Generate inviteCode.
-        const { manager, player } = body as any;
-        const data = JSON.stringify({
-            charName: player,
-            manager,
-        });
-
-        if (!player || !manager) {
-            ctx.status = 400;
-            ctx.body = '錯誤的角色名稱';
-            return;
+router.post('/invite-code', async (ctx, next) => {
+    await levelIdentify(ctx, () => {
+        const body = ctx.request.body;
+    
+        if (body) {
+            // Generate inviteCode.
+            const { manager, player } = body as any;
+            const data = JSON.stringify({
+                charName: player,
+                manager,
+            });
+    
+            if (!player || !manager) {
+                ctx.status = 400;
+                ctx.body = '錯誤的角色名稱';
+                return;
+            }
+    
+            const inviteCode = Buffer.from(data, 'utf8').toString('hex');
+    
+            const path = './src/views/admin/invite-code.pug';
+            const html = renderHtml(path, { manager, player, inviteCode, webAppURL });
+            ctx.body = html;
         }
-
-        const inviteCode = Buffer.from(data, 'utf8').toString('hex');
-
-        const path = './src/views/admin/invite-code.pug';
-        const html = renderHtml(path, { manager, player, inviteCode, webAppURL });
-        ctx.body = html;
-    }
+    });
 });
 
 export { router };
