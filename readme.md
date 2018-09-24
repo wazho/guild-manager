10eaa8b9af7aa39d521a454a4449812c1e8e3f1e
Ze-Hao Wang (a.k.a. Salmon) <salmon.zh.tw@gmail.com>
2018-09-24 09:01:43 +0800

    (+) Add readme.

---
+++ readme.md
@@ -0,0 +1,50 @@
+
+# Requirements
+
+## LINE Login settings
+
+1. Go to LINE developer https://developers.line.me/
+2. Add new provider or use exist provider
+3. Create new channel > LINE Login
+4. [tab] App settings > Enter your 'Callback URL' same with in `./src/config.ts`
+5. [tab] Channel settings > Paste 'Channel ID' and 'Channel secret' in `./src/config.ts`
+
+
+## Google APIs settings
+
+### API keys
+
+Quick access (readonly) to Google spreadsheet without other auth by using API keys.
+
+1. Go to Google APIs https://console.developers.google.com/
+2. New Project (if you don't have anyone)
+3. Select your project > Credentials
+4. Create credentials > API key > Restrict key
+5. [tab] Application restrictions > None (dev) / HTTP referrers (prod)
+6. [tab] API restrictions > Google Sheets API > Save
+7. Copy and paste the key to 'apiKey' below
+8. Go to Google Drive https://www.google.com/drive/
+9. Create an spreadsheet, then copy ID and sheet in `./src/config.ts`
+
+### OAuth 2.0 client IDs
+
+Offline access to your spreadsheet. Notice to protect your `credentials`, it has complete permission for your Google Spreadsheet.
+
+1. Follow above, create another credential > OAuth client ID > Other
+2. Click 'Download JSON' of this row
+3. Move this file to `./credentials.json`
+
+
+# Launching
+
+## Run web server
+
+```shell
+$ yarn
+$ yarn start
+# Visit http://localhost:3000
+```
+
+## Initial your environment
+
+under construction
--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,29 @@
+/**
+ * Step 1: Go to LINE developer https://developers.line.me/
+ * Step 2: Add new provider or use exist provider
+ * Step 3: Create new channel > LINE Login
+ * Step 4: [tab] App settings > Enter your 'Callback URL' same with below
+ * Step 5: [tab] Channel settings > Paste 'Channel ID' and 'Channel secret' below
+ */
 export const lineChannel = {
-    // Just a test channel.
-    channelID: '1608000703',
-    channelSecret: '2eaa8b15b3a76849bbe01aaab34528fc',
-    callbackURL: 'http://192.168.0.100:3000/auth/line/callback',
+    channelID: 'your-channel-id',
+    channelSecret: 'your-channel-secret',
+    callbackURL: 'http://your-domin/auth/line/callback',
 };
 
+/**
+ * Step 1: Go to Google APIs https://console.developers.google.com/
+ * Step 2: New Project (if you don't have anyone)
+ * Step 3: Select your project > Credentials
+ * Step 4: Create credentials > API key > Restrict key
+ * Step 5: [tab] Application restrictions > None (dev) / HTTP referrers (prod)
+ * Step 6: [tab] API restrictions > Google Sheets API > Save
+ * Step 7: Copy and paste the key to 'apiKey' below
+ * Step 8: Go to Google Drive https://www.google.com/drive/
+ * Step 9: Create an spreadsheet, then copy ID and sheet below
+ */
 export const googleApis = {
-    apiKey: 'AIzaSyDODbcPAYSRM5Hc1A7idaKpsSw9SlTlWVE',
-    spreadsheetId: '17vy4xTlfBe0YZeRB2DygK22KJpCZW27reoEUl46TX7g',
-    sheetName: 'members',
+    apiKey: 'your-api-key',
+    spreadsheetId: 'your-spreadsheet-id',
+    sheetName: 'your-name-of-sheet',
 }
